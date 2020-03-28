7aadfbbb389ddd1d526aa33b81bf85c264ff7c11
mrtompkin18 <jungraiman56@gmail.com>
2020-03-28 18:55:10 +0700

    edit api

--- src/api/review.api.js
+++ src/api/review.api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 export const fetchReview = async () => {
-    return await axios.get("http://www.mocky.io/v2/5e1d227c3600002a00c73e82")
+    return await axios.get("https://www.mocky.io/v2/5e1d227c3600002a00c73e82")
         .then(res => res).catch(error => error);
 }
 
 export const fetchTopic = async () => {
-    return await axios.get("http://www.mocky.io/v2/5e1d24dd3600005a00c73e8c")
+    return await axios.get("https://www.mocky.io/v2/5e1d24dd3600005a00c73e8c")
         .then(res => res).catch(error => error);
 }
